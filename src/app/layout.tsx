885dce845d5276485cba5da05a642db9bb3bd2c9
Emre Arslan <emrearslanea8@gmail.com>
2026-02-08 14:57:40 +0300

    refactor: remove unused icon image and update layout metadata to reflect changes in icon sources

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -94,10 +94,7 @@ export const generateMetadata = async () => ({
   applicationName: "Mitenya",
   manifest: "/manifest.webmanifest",
   icons: {
-    icon: [
-      { url: "/favicon.ico", sizes: "48x48", type: "image/x-icon" },
-      { url: "/static/images/icon.png", sizes: "67x67", type: "image/png" },
-    ],
+    icon: [{ url: "/favicon.ico", sizes: "48x48", type: "image/x-icon" }],
     shortcut: ["/favicon.ico"],
     apple: [{ url: "/static/images/apple-touch-icon.png", type: "image/png" }],
   },
