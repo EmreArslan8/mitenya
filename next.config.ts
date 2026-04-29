b79dd702750c960d88df05df0f7f2c9561f10f3d
Emre Arslan <emrearslanea8@gmail.com>
2026-04-29 20:51:19 +0300

    chore: update Content Security Policy to include additional AWS and Google Cloud domains for improved resource connectivity

--- next.config.ts
+++ next.config.ts
@@ -78,7 +78,7 @@ const nextConfig = {
                 "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
                 imgSrc + " https://www.google-analytics.com https://www.googletagmanager.com https://www.facebook.com https://analytics.tiktok.com https://ads.tiktok.com",
                 "font-src 'self' data: https://fonts.gstatic.com",
-                "connect-src 'self' https://*.supabase.co https://www.paytr.com https://www.google-analytics.com https://*.analytics.google.com https://www.googletagmanager.com https://challenges.cloudflare.com https://connect.facebook.net https://www.facebook.com https://analytics.tiktok.com https://ads.tiktok.com https://*.ingest.de.sentry.io https://*.ingest.sentry.io",
+                "connect-src 'self' https://*.supabase.co https://www.paytr.com https://www.google-analytics.com https://*.analytics.google.com https://www.googletagmanager.com https://challenges.cloudflare.com https://connect.facebook.net https://www.facebook.com https://analytics.tiktok.com https://ads.tiktok.com https://*.ingest.de.sentry.io https://*.ingest.sentry.io https://*.ecs.us-east-2.on.aws https://*.us-central1.run.app",
                 "frame-src https://www.paytr.com https://challenges.cloudflare.com https://www.googletagmanager.com https://www.facebook.com https://analytics.tiktok.com https://ads.tiktok.com",
                 "frame-ancestors 'self'",
                 "form-action 'self' https://www.facebook.com",
